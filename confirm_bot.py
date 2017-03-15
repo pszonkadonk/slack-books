0052295aef36139f416ce9adf70cbde950d444dc
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-03-15 12:09:31 -0400

    Instantiate Slackbot

---
+++ confirm_bot.py
@@ -0,0 +1,18 @@
+import os
+from slackclient import SlackClient
+
+
+BOT_NAME = 'slack-bot'
+
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+
+if __name__ == '__main__':
+    api_call_users = slack_client.api_call("users.list")
+    if api_call_users.get("ok"):
+        users = api_call_users.get("members")
+        for user in users:
+            if "name" in user and user.get("name") == BOT_NAME:
+                print("BOT ID for " + user["name"] + " is " + user.get("id"))
+            else:
+                print("There is no bot user with the name " + BOT_NAME)
