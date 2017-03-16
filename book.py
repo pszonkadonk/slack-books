fd9fe8d9f336ae00b86d8386d612cacdddb97b83
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-03-16 15:51:38 -0400

    handle genre and author messaging

--- book.py
+++ book.py
@@ -1,6 +1,6 @@
 import requests
+from xml.etree import ElementTree
 import os
-from dotenv import dot_env
 
 
 class BookClient:
@@ -9,27 +9,56 @@ class BookClient:
         self.api_key = goodreads_key
 
 
-
-
-
     def find_by_author(self, author):
 
+        url = self.endpoint+'/search/index.xml?'
+
         params = {
-            "key": self.api_key
-            "q": author,
+            "key": self.api_key,
+            "q": author
         }
 
-        return requests.get(self.endpoint, params=params)
+        results = requests.get(url, params=params)
+        root = ElementTree.fromstring(results.content)
+
+        book_list = self.get_array_of_works(root)
+
+        return book_list
+
 
 
     def find_by_genre(self, genre):
 
         params = {
-            "key": self.api_key
-            "q": genre,
+            "key": self.api_key,
+            "q": genre
         }
 
         return requests.get(self.endpoint, params=params)
 
 
+    def get_array_of_works(self, xmlResult):
+        work_list = []
+        for work in xmlResult.iter('work'):
+            work_dict = {}
+            work_dict['book_id'] = work[0].text
+            work_dict['review_count'] = work[3].text
+            work_dict['publication_year'] = work[4].text
+            work_dict['average_rating'] = work[7].text
+
+            best_book = work[8]
+            work_dict['book_title'] = best_book[1].text
+            
+            author = best_book[2]
+
+            work_dict['author_id'] = author[0].text
+            work_dict['author_name'] = author[1].text
+
+            work_list.append(work_dict)
+        
+        return work_list
     
+    def get_book_info_by_id(self, book_id):
+
+
+        # do things
--- slackbook.py
+++ slackbook.py
@@ -1,36 +1,81 @@
 import os
 import time
 from slackclient import SlackClient
+from book import BookClient
 
 class SlackBook:
-    def __init__(self, bot_id, slack_client, watson_conversation, goodreads_client):
+    def __init__(self, bot_id, slack_client, watson_conversation, book_client):
 
         self.bot_id = bot_id
         self.slack_cleint = slack_client
         self.watson_conversation = watson_conversation
-        self.goodreads_client = goodreads_client
+        self.book_client = book_client
         self.delay = 1
         self.workspace_id = 'c1a2722a-b334-4b16-a197-ec4e293d5fc9'
 
         self.bot_directed = "<@" + bot_id + ">"
 
-    def handle_message(self, command, channel):
+    def format_book_info(self, book_description, book_rating):
+
+        #do things
+
+    def handle_selection_message(self, selection):
+        book_id = self.context['books'][selection-1]['book_id']
+        book_description = self.book_client.get_book_info_by_id(book_id)
+        book_rating = self.context['books'][selection-1]['average_rating']
+
+        return self.format_book_info(book_description, book_rating)
+
+
+    def handle_message(self, message, channel):
         """
         Function is called whenever an individual directs a message at bot.
-        If the command is valid, the bot will act on the command.  If it is not,
+        If the message is valid, the bot will act on the message.  If it is not,
         the bot will respond make, asking the user to rephrase their request.
         """
         watson_response = self.watson_conversation.message(
             workspace_id = self.workspace_id
-            message_input = {"text": command}
+            message_input = {"text": message}
             context = self.context)
-            
-        response = "Sorry, I'm not sure what you are asking for.  Please rephrase your question."
 
-        if(command.startswith(EXAMPLE_COMMAND)):
-            response = "sure write some more and I can do that"
+        self.context = watson_response["context"]
+
+        if 'is_author' in self.context.keys() and self.context['is_author']:
+            response = self.handle_author_message(message)
+
+        elif 'is_selection' in self.context.keys() and self.context['is_selection']:
+            response = "Invalid entry. Press any key to see your choices again"
+            
+            if self.context['selection'].isdigit():
+                selection = int(self.context['selection'])
+                if selection >= 1 and selection <= 20:
+                    self.context['seletion_valid'] = True
+                    response = self.handle_selection_message(selection)
+
+        elif watson_response['entities'] and watson_response['entities'][0]['entity'] == 'genre':
+            genre = watson_response['entities'][0]['value']
+            response = self.handle_genre_message(genre)
+
+        else:
+            response = ''
+            for text in watson_response['output']['text']:
+                response += text + "\n"            
         
         self.respond_to_message(response, channel)
+
+        
+
+    def handle_author_message(self, message):
+        if self.context['get_books']:
+            self.context['books'] = self.book_client.find_by_author(message)
+
+        response = "Ive found the following books for that author: \n"
+
+        for i, book in enumerate(self.context['books']):
+            response += str(i+1) + ". " + book['book_title'] + "\n"
+        response+= "Please enter the number of the book you would like to know more about"
+
+        return response
 
 
     def parse_output(self, slack_rtm_output):
@@ -63,4 +108,5 @@ class SlackBook:
                 time.sleep(DELAY)
         else:
             print("Could not connect to slack.  Please check slack api token or bot id")
+
 
