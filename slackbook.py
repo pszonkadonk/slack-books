361df6683a7887ec9de69ac8e05f38598de2157c
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-03-16 16:33:58 -0400

    Refactor

--- slackbook.py
+++ slackbook.py
@@ -44,8 +44,10 @@ class SlackBook:
             response = self.handle_author_message(message)
 
         elif 'is_selection' in self.context.keys() and self.context['is_selection']:
-            response = "Invalid entry. Press any key to see your choices again"
             
+            self.context['selection_valid'] = False
+            response = "Invalid entry. Press any key to see your choices again..."
+
             if self.context['selection'].isdigit():
                 selection = int(self.context['selection'])
                 if selection >= 1 and selection <= 20:
